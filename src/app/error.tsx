0a57a630702d5c3eede105a3b94ffa9cbe96c6b5
Lee Gyumin <legm0310@gmail.com>
2025-04-21 20:25:31 +0900

    Feat: 글로벌 파일, 루트 레이아웃, 에러 페이지

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Internal Error</h1>
+      <p>잠시 후 다시 이용해주세요.</p>
+    </div>
+  );
+};
+
+export default Error;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from 'next';
-
 import '@/styles/globals.css';
-import localFont from 'next/font/local';
-import { FONT_PRETENDARD, FONT_SF_PRO } from '@/fonts/font';
 import Header from '@/components/layouts/header/Header';
 import Footer from '@/components/layouts/Footer';
 import ThemeProvider from '@/components/layouts/header/theme-toggle/Provider';
+import Toaster from '@/components/ui/Toaster';
 
 export const metadata: Metadata = {
   title: '사나이클럽',
@@ -20,10 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen">
+        {/* <body className="flex flex-col min-h-screen "> */}
         <ThemeProvider>
           <Header />
           <main className="flex-1">{children}</main>
+          {/* <main className="mt-[64px] flex flex-1 flex-col">{children}</main> */}
           <Footer />
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
