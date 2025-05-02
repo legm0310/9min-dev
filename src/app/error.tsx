37d5f279a6a50d7b2d182f49af496c0a2e965325
Lee Gyumin <legm0310@gmail.com>
2025-05-02 20:38:52 +0900

    Feat: 에러 페이지 스타일

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,11 +1,15 @@
 'use client';
 
+import PageCotainer from '@/components/layouts/PageContainer';
+
 const Error = () => {
   return (
-    <div>
-      <h1>Internal Error</h1>
-      <p>잠시 후 다시 이용해주세요.</p>
-    </div>
+    <PageCotainer className="w-full">
+      <div className="flex flex-1 flex-col items-center">
+        <h1 className="mt-16">Internal Error</h1>
+        <p className="mt-4">잠시 후 다시 이용해주세요.</p>
+      </div>
+    </PageCotainer>
   );
 };
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
+import PageCotainer from '@/components/layouts/PageContainer';
+
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
-      <p>페이지를 찾을 수 없습니다.</p>
-    </div>
+    <PageCotainer className="w-full">
+      <div className="flex flex-1 flex-col items-center">
+        <h1 className="mt-16"> Page Not Found</h1>
+        <p className="mt-4">페이지를 찾을 수 없습니다.</p>
+      </div>
+    </PageCotainer>
   );
 };
 
