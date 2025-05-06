c443dd05e52c6e1bdf53a5ea083c4b5bbd023099
Lee Gyumin <legm0310@gmail.com>
2025-05-07 06:38:24 +0900

    Feat: 게시물 리스트 없을 시 텍스트 렌더링

--- src/components/posts/list/PostCardList.tsx
+++ src/components/posts/list/PostCardList.tsx
@@ -3,15 +3,20 @@ import PostCard from './PostCard';
 
 interface PostCardListProps {
   posts: PostSummary[];
+  className?: string;
 }
 
-const PostCardList = ({ posts }: PostCardListProps) => {
-  return (
-    <div className="list-grid-only-col1">
+const PostCardList = ({ posts, className = '' }: PostCardListProps) => {
+  const hasPost = posts.length === 0 ? false : true;
+
+  return hasPost ? (
+    <div className={`list-grid-only-col1 ${className}`}>
       {posts.map((post) => (
         <PostCard key={post.slug} postInfo={post} />
       ))}
     </div>
+  ) : (
+    <h3>업로드된 게시물이 없습니다..</h3>
   );
 };
 
