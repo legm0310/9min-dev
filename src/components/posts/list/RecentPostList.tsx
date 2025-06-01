f850f297e129a38798d965ad32159d76ab0dd5be
Lee Gyumin <legm0310@gmail.com>
2025-06-01 23:25:21 +0900

    Fix: 최근 게시물 목록 개인 게시물 필터링

--- src/components/posts/list/RecentPostList.tsx
+++ src/components/posts/list/RecentPostList.tsx
@@ -2,7 +2,8 @@ import { getPostSummaryList } from '@/lib/post';
 import PostCardList from './PostCardList';
 
 const RecentPostList = async () => {
-  const posts = await getPostSummaryList();
+  let posts = await getPostSummaryList();
+  posts = posts.filter((post) => post.category !== 'admin');
   return (
     <section>
       <div className="mb-10 section-heading-semibold">Recent Posts</div>
