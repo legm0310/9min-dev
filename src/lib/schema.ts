a96e1df7350337c31b86ce540cf9f95f44ce7c4c
Lee Gyumin <legm0310@gmail.com>
2025-06-16 04:11:00 +0900

    Refactor: frontmatter 수정

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -2,8 +2,9 @@ import { z } from 'zod';
 
 export const PostFrontmatterSchema = z.object({
   title: z.string(),
-  date: z.coerce.date(),
+  date: z.string(),
   category: z.string(),
-  tags: z.array(z.string()),
-  thumbnail: z.string(),
+  tags: z.array(z.string()).default([]),
+  thumbnail: z.string().default(''),
+  description: z.string().default(''),
 });
