792b6cd129859699609bdc7fe9f259bfa2e45c8a
Lee Gyumin <legm0310@gmail.com>
2025-07-10 21:54:25 +0900

    Feat: 태그 UI

--- src/components/posts/list/TagFilter.tsx
+++ src/components/posts/list/TagFilter.tsx
@@ -1,3 +1,20 @@
-const TagFilter = () => {};
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const TagFilter = () => {
+  const [tags, setTags] = useState<string[]>([]);
+
+  return (
+    <div>
+      <h3>Tags</h3>
+      <div className="flex flex-col">
+        {tags.map((tag) => (
+          <div key={tag}>{tag}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TagFilter;
